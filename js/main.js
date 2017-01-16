9c12b69b5b50f22389f2a60d73d2c57b46b22289
Muthu Annamalai Chidambaram <muthuannamalai14058@cse.ssn.edu.in>
2017-01-16 23:07:45 +0530

    Landing Page Done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+   $(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
